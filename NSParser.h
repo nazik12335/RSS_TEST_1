9914482795676272aa2d46e425938da5bd02a90f
nazar <nazar@ip-188-230-41-156.airbites.net.ua>
2015-04-06 17:33:35 +0300

    first commit

---
+++ NSParser.h
@@ -0,0 +1,15 @@
+//
+//  NSParser.h
+//  RSS_TEST_1
+//
+//  Created by nazar on 4/6/15.
+//  Copyright (c) 2015 Nazand. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSParser : NSObject <NSXMLParserDelegate>
+
+@property (strong, nonatomic) NSMutableData
+
+@end
---
+++ NSParser.m
@@ -0,0 +1,13 @@
+//
+//  NSParser.m
+//  RSS_TEST_1
+//
+//  Created by nazar on 4/6/15.
+//  Copyright (c) 2015 Nazand. All rights reserved.
+//
+
+#import "NSParser.h"
+
+@implementation NSParser 
+
+@end
